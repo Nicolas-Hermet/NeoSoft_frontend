68a68e09e2e4ba05f9a6a22eae3961d7ea0ced8d
Nicolas Hermet <n.hermet@gmail.com>
2019-05-25 16:38:00 +0200

    feat(list of todos): create a todo model for the component

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoModel } from '../models/todo.model';
 
 @Component({
   selector: 'app-todos',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.sass']
 })
 export class TodosComponent implements OnInit {
+
+  todos: Array<TodoModel> = [
+    {title: 'shopping list', description: 'Milk'},
+    {title: 'Technical test', description: 'spend some time each day to make things happen !'}
+  ];
 
   constructor() { }
 
