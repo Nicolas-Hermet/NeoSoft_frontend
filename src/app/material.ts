c3aa354b4723126aa112b5a365445c133d7207c9
Nicolas Hermet <n.hermet@gmail.com>
2019-05-25 16:36:45 +0200

    feat(list of todos): import material cards

--- src/app/material.ts
+++ src/app/material.ts
@@ -1,9 +1,16 @@
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  imports: [MatToolbarModule],
-  exports: [MatToolbarModule]
+  imports: [
+    MatToolbarModule,
+    MatCardModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatCardModule
+  ]
 })
 
 export class MaterialModule { }
