ed068e1a7608d059d2406e47fe657acc2597af8b
Nicolas Hermet <n.hermet@gmail.com>
2019-05-27 11:22:48 +0200

    feat(list of todos): create services to dispatch mock data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material';
 import { MenuComponent } from './menu/menu.component';
 import { TodosComponent } from './todos/todos.component';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { DataService } from './services/data.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -19,7 +22,9 @@ import { TodosComponent } from './todos/todos.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    HttpClientModule,
+    InMemoryWebApiModule.forRoot(DataService)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/todos.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { TodoModel } from '../models/todo.model';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodosService {
+
+  todos: Observable<Array<TodoModel>>;
+  SERVER_URL = 'http://localhost:8080/api/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getTodos() {
+    return this.httpClient.get<Array<TodoModel>>(this.SERVER_URL + 'todos');
+  }
+
+
+}
