3b81aaeb0df79bebbf45caa177817c382d2b9b53
Nicolas Hermet <n.hermet@gmail.com>
2019-06-11 12:11:49 +0200

    feat(list of todos): use ngrx to get list of todos in component

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,23 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+
 import { TodoModel } from '../models/todo.model';
 import { TodosService } from '../services/todos.service';
+
+import { Observable, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { Store, select, Action } from '@ngrx/store';
+import { ToDoState } from '../redux/todo.state';
+import ActionWithPayload from '../redux/ActionWithPayload';
+import { GET_TODO } from '../redux/todo.actions';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.sass']
 })
-export class TodosComponent implements OnInit {
+export class TodosComponent implements OnInit, OnDestroy {
 
   todos: Array<TodoModel>;
 
-  constructor(private todosService: TodosService) {
+  toDoState$: Observable<ToDoState>;
+  toDoSubscription: Subscription;
+  @Input() title: string;
+  @Input() done = false;
+  description: string;
+
+  constructor(private store: Store<ToDoState>) {
   }
 
   ngOnInit() {
-    this.todosService.getTodos().subscribe((data: Array<TodoModel>) => {
-      console.log(data);
-      this.todos = data;
-    });
+    this.toDoState$ = this.store.pipe(select('todos'));
+
+    const getToDoAction: Action = {
+      type: GET_TODO
+    };
+    this.toDoState$.pipe(map((x) => {
+      this.todos = x.todos;
+    })).subscribe();
+    this.store.dispatch(getToDoAction);
+  }
+
+  ngOnDestroy() {
+      this.toDoSubscription.unsubscribe();
   }
 }
