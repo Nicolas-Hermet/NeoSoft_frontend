e4507c7fdd9b4b6f8a3a87d47ac5738e6efc7143
Nicolas Hermet <n.hermet@gmail.com>
2019-06-18 12:02:04 +0200

    fix(change a todo state): prevent from adding duplicate element to the state

--- src/app/redux/todo.reducer.ts
+++ src/app/redux/todo.reducer.ts
@@ -9,22 +9,32 @@ const initialState = initializeState();
 export function ToDoReducer(state: ToDoState = initialState,
                             action: Action) {
 
+  let actionWithPayload: Array<any>;
+
   switch (action.type) {
       case ToDoActions.GET_TODO:
-          return { ...state };
+        return { ...state };
 
-      case ToDoActions.GET_TODO_SUCCESS:
-          return ({
-              ...state,
-              todos: state.todos.concat((action as ActionWithPayload<TodoModel[]>).payload)
-          });
+        case ToDoActions.GET_TODO_SUCCESS:
+          actionWithPayload = (action as ActionWithPayload<TodoModel[]>).payload;
+          if (actionWithPayload.length >= state.todos.length) {
+            return ({
+                ...state,
+                todos: state.todos.concat((action as ActionWithPayload<TodoModel[]>).payload)
+            });
+          } else {
+            state.todos.map((x) => x.id === actionWithPayload.id ? x =  actionWithPayload : x);
+            return  ({
+              ...state
+            });
+          }
 
       case ToDoActions.CHANGE_TODO:
-          const actionWithPayload = (action as ActionWithPayload<TodoModel>).payload;
-          return ({ ...state,
-            todos: state.todos.map((todo: TodoModel) =>
+          actionWithPayload = (action as ActionWithPayload<TodoModel>).payload;
+          state.todos.map((todo: TodoModel) =>
               todo.id === actionWithPayload.id ? { ...todo, isDone: !todo.isDone } : todo
-            ) });
+            );
+          return ({ ...state });
 
 
       case ToDoActions.CHANGE_TODO_SUCCESS:
