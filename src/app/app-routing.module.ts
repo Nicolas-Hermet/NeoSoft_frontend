76aff27ca8cf0423e3d36497d9ffd7a26eac7378
Nicolas Hermet <n.hermet@gmail.com>
2019-07-02 10:52:52 +0200

    feat(detail a todo): create routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { TodosComponent } from './todos/todos.component';
+import { TodoComponent } from './todos/todo/todo.component';
+
+const routes: Routes = [
+  { path: '', children: [
+    {path: '', component: TodosComponent},
+    {path: 'todo/:todoId', component: TodoComponent}
+  ]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
