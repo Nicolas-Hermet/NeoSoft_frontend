5c7ed36e88ccc90e312a54d03b9a4de577584dd1
Nicolas Hermet <n.hermet@gmail.com>
2019-07-15 09:57:35 +0200

    feat(todos order): implement reverse pipe for the todos list

--- src/app/pipes/reverse.pipe.ts
+++ src/app/pipes/reverse.pipe.ts
@@ -1,12 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { TodoModel } from '../models/todo.model';
 
 @Pipe({
   name: 'reverse'
 })
 export class ReversePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: Array<TodoModel>, args?: any): any {
+    return value.sort( (a, b) => {
+      return b.id - a.id;
+    });
   }
 
 }
